525585f6c10696e1917194333b7dc6ae9f84c299
Agung Baptiso Sorlawan <agung.sorlawan@prosa.ai>
2020-04-19 16:57:15 +0700

    Separate handlers

---
+++ app/handlers/books.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"library/app/utils/log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+)
+
+type Book struct {
+	Title         string    `json:"title" validate:"required"`
+	Author        string    `json:"author" validate:"required"`
+	PublishedDate time.Time `json:"published_date"`
+}
+
+func HandleBook(c echo.Context) (err error) {
+	book := new(Book)
+	if err = c.Bind(book); err != nil {
+		return
+	}
+	if err = c.Validate(book); err != nil {
+		return
+	}
+	log.Info("Book", zap.String("Title", book.Title))
+	book.PublishedDate = book.PublishedDate.Add(1000 * time.Hour)
+	return c.JSON(http.StatusOK, book)
+}
